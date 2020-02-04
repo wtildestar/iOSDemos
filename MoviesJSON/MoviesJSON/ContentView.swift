e99ef7bbb874dc5b3250dbaa8096614968bbb18c
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-02-04 23:20:21 +0500

    Create Movie model

---
+++ MoviesJSON/MoviesJSON/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoviesJSON
+//
+//  Created by wtildestar on 04/02/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoviesJSON/MoviesJSON/Movie/Movie.swift
@@ -0,0 +1,47 @@
+//
+//  Movie.swift
+//  MoviesJSON
+//
+//  Created by wtildestar on 04/02/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import Foundation
+
+struct MoviesResponse: Codable {
+    let page: Int
+    let totalResults: Int
+    let totalPages: Int
+    let results: [Movie]
+}
+
+struct Movie: Identifiable, Codable {
+    let id: Int
+    let title: String
+    let overview: String
+    let posterPath: String?
+    let releaseDate : Date
+}
+
+//------------
+
+struct MovieCreditResponse: Codable {
+    let cast: [MovieCast]
+    let crew: [MovieCrew]
+}
+
+struct MovieCast: Identifiable, Codable {
+    let id: Int
+    let character: String
+    let name: String
+    let profilePath: String?
+}
+
+struct MovieCrew: Identifiable,Codable {
+    let id: Int
+    let department: String
+    let job: String
+    let name: String
+}
+
+
