60323f84217a192baa01aef4cfbe1b0d0139f598
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-02-06 21:40:47 +0500

    Initial commit

---
+++ GHFollowers/GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by wtildestar on 06/02/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
