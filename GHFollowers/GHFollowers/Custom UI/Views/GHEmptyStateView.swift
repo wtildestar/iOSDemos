265cb992dd92a9a90c6da6147a790c971a3f12c8
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-02-17 23:51:09 +0500

    Empty state view

---
+++ GHFollowers/GHFollowers/Custom UI/Views/GHEmptyStateView.swift
@@ -0,0 +1,49 @@
+//
+//  GHEmptyStateView.swift
+//  GHFollowers
+//
+//  Created by wtildestar on 17/02/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class GHEmptyStateView: UIView {
+    
+    let messageLabel = GHTitleLabel(textAlignment: .center, fontSize: 28)
+    let logoImageView = UIImageView()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(messageLabel)
+        addSubview(logoImageView)
+        
+        messageLabel.numberOfLines  = 3
+        messageLabel.textColor      = .secondaryLabel
+        
+        logoImageView.image         = UIImage(named: "empty-state-logo")
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: -150),
+            messageLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 40),
+            messageLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -40),
+            messageLabel.heightAnchor.constraint(equalToConstant: 200),
+            
+            logoImageView.widthAnchor.constraint(equalTo: self.widthAnchor, constant: 1.3),
+            logoImageView.heightAnchor.constraint(equalTo: self.widthAnchor, constant: 1.3),
+            logoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 200),
+            logoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 140)
+        ])
+    }
+    
+}
