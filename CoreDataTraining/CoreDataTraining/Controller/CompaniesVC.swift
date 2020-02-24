fc88f40c2258f1593543ad1e162aa2c0a5291fff
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-02-24 23:35:07 +0500

    Initial commit

---
+++ CoreDataTraining/CoreDataTraining/Controller/CompaniesVC.swift
@@ -0,0 +1,19 @@
+//
+//  CompaniesVC.swift
+//  CoreDataTraining
+//
+//  Created by wtildestar on 24/02/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class CompaniesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .yellow
+    }
+
+}
