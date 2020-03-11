ff6c6b698d32d44e0edd24aaea563f198327ebb0
wtildestar <31348327+wtildestar@users.noreply.github.com>
2020-03-11 20:34:56 +0500

    Preparing to receive photos in CollectionViewCell

---
+++ RxSwift/PhotoFilter/PhotoFilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoFilter
+//
+//  Created by wtildestar on 11/03/2020.
+//  Copyright © 2020 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+    }
+}
+
